843c47bccfb4656e183c12de64a89b9e142bc9bd
Mederic <32560642+mederic-p@users.noreply.github.com>
2018-04-25 14:06:18 +0700

    Add travis integration (#38)

--- OmiseGOTests/ToolsTests.swift
+++ OmiseGOTests/ToolsTests.swift
@@ -15,8 +15,8 @@ class ToolsTests: XCTestCase {
         let date = Date(timeIntervalSince1970: 0)
         let defaultFormat = date.toString(timeZone: TimeZone(secondsFromGMT: 0))
         XCTAssertEqual(defaultFormat, "1970-01-01T00:00:00Z")
-        let customFormat = date.toString(withFormat: "yyyy MM dd HH:mm:ss")
-        XCTAssertEqual(customFormat, "1970 01 01 07:00:00")
+        let customFormat = date.toString(withFormat: "yyyy MM dd HH:mm:ss", timeZone: TimeZone(secondsFromGMT: 0))
+        XCTAssertEqual(customFormat, "1970 01 01 00:00:00")
     }
 
     func testDecodingContainerProtocol() {
